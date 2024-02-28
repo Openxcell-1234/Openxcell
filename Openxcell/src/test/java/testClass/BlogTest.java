69ec1c2ba96684bdfd7ac7b173e849a545795afd
Openxcell-1234 <161296995+Openxcell-1234@users.noreply.github.com>
2024-02-28 14:17:20 +0530

    vbvb

--- Openxcell/src/test/java/testClass/BlogTest.java
+++ Openxcell/src/test/java/testClass/BlogTest.java
@@ -8,15 +8,6 @@ public class BlogTest extends BeforeAfterTest{
 	public void method() {
 		System.out.println("Hello");
 	}
-	
-	@Test
-	public void method_96() {
-		System.out.println("Hello");
-	}
-	
-	@Test
-	public void method_56() {
-		System.out.println("Hello");
-	}
+
 
 }
